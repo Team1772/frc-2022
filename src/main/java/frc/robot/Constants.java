362ce3eedf18380394cc654260b2bdbe2928b20e
Nathany <nathanysanti.nm@gmail.com>
2022-01-20 22:07:02 -0300

    hanger

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final class HangerConstants {
+        public static final int[]
+			motorsPorts = new int[] { 2, 3 };
+
+    }
+
+}
