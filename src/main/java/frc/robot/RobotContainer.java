b266ed5540c950b3f1208d9ddebe803da9e4444d
Nathany <nathanysanti.nm@gmail.com>
2022-01-20 21:53:06 -0300

    created hangar

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,48 +1,42 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.HangerDown;
+import frc.robot.commands.HangerUp;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Hanger;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private Hanger hanger;
+  private XboxController operator;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
+    this.hanger = new Hanger();
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    this.configureButtonBindingsHanger();
+  }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+  private void configureButtonBindingsHanger() {
+    var buttonA = new JoystickButton(this.operator, Button.kA.value);
+    var buttonB = new JoystickButton(this.operator, Button.kB.value);
+
+    buttonA
+      .whileHeld(new HangerDown(this.hanger));
+    
+    buttonB
+      .whileHeld(new HangerUp(this.hanger));
+  }
+
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/Hanger.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.HangerConstants;
+
+
+public class Hanger extends SubsystemBase {
+  private VictorSPX motorOne, motorTwo;
+
+  public Hanger() {
+    motorOne = new VictorSPX(HangerConstants.motorsPorts[0]);
+    motorTwo = new VictorSPX(HangerConstants.motorsPorts[1]);
+  }
+
+  public void set(double speedOne, double speedTwo) {
+    this.motorOne.set(ControlMode.PercentOutput, speedOne);
+    this.motorTwo.set(ControlMode.PercentOutput, speedTwo);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+}
