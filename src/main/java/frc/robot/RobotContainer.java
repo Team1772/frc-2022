5bd38a37d622c3c5430a414551faff075e48546b
Bialves <biancadsalves@gmail.com>
2022-01-21 20:21:12 -0300

    Deleted buttons and created ShooterConstants class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,46 +1,17 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Axis;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.PullCargo;
-import frc.robot.commands.ShootCargo;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final Shooter shooter;
-  private final XboxController operator;
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   public RobotContainer() {
-    this.shooter = new Shooter();
-    this.operator = new XboxController(0);
-   
+    
     configureButtonBindings();
   }
 
-  private void configureButtonBindings() {
-    configureButtonBindingsShooter();
-  }
-
-  private void configureButtonBindingsShooter(){
-    var axisTriggerRight = new JoystickButton(this.operator, Axis.kRightTrigger.value);
-    var buttonBumperRight = new JoystickButton(this.operator, Button.kRightBumper.value);
-    
-    axisTriggerRight
-      .whileHeld(new ShootCargo(this.shooter));
-
-    buttonBumperRight
-      .whileHeld(new PullCargo(this.shooter));
-  }
+  private void configureButtonBindings() {}
 
   public Command getAutonomousCommand() {
-    return m_autoCommand;
+    return null;
   }
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,12 +3,13 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
   private final TalonSRX motor;
 
   public Shooter() {
-    this.motor = new TalonSRX(0);
+    this.motor = new TalonSRX(Constants.ShooterConstants.motorPort);
   }
 
   public void setSpeed(double speed){
