378402eb5bf82584515e2b5c7ddbfe4ea275ea49
Bialves <biancadsalves@gmail.com>
2022-01-25 22:08:38 -0300

    Fixed Shooter Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@ public final class Constants {
   public static final class ShooterConstants {
     public static final int motorPort = 0;
 
+    public static final double wheelRadius = 0.0762;
+
     public static final int maxRPM = 6232;
 
     public static final boolean 
