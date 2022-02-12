d60c27dcf6c968761fee0a64caa8580ca12e0cf9
TheBrazilianTrailblazers <thebraziliantrailblazers.github@gmail.com>
2022-02-12 14:09:23 -0300

    test with raw ramsete

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,23 @@
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.core.util.TrajectoryBuilder;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.DrivetrainConstants.PID;
 import frc.robot.commands.autonsTrajectory.Test;
 import frc.robot.commands.drivetrain.ArcadeDrive;
 import frc.robot.subsystems.Drivetrain;
@@ -38,7 +52,39 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return new Test(this.trajectoryBuilder);
+    // return new Test(this.trajectoryBuilder);
+    String trajectoryJSON = "paths/output/test.wpilib.json";
+    Trajectory trajectory = new Trajectory();
+
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+    }
+    
+    RamseteCommand ramseteCommand =
+        new RamseteCommand(
+            trajectory,
+            this.drivetrain::getPose,
+            new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+            new SimpleMotorFeedforward(
+                DrivetrainConstants.ksVolts,
+                DrivetrainConstants.kvVoltSecondsPerMeter,
+                DrivetrainConstants.kaVoltSecondsSquaredPerMeter),
+            DrivetrainConstants.kDriveKinematics,
+            this.drivetrain::getWheelSpeeds,
+            new PIDController(PID.kPDriveVelocity, 0, 0),
+            new PIDController(PID.kPDriveVelocity, 0, 0),
+            // RamseteCommand passes volts to the callback
+            this.drivetrain::tankDriveVolts,
+            this.drivetrain);
+
+    // Reset odometry to the starting pose of the trajectory.
+    this.drivetrain.resetOdometry(trajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return ramseteCommand.andThen(() -> this.drivetrain.tankDriveVolts(0, 0));
   }
 
   public void reset() {
